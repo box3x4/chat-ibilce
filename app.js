78201978d15645d72e1aea77f8f05c7c91ba1954
Rafael Carreira <rafael_carreira11@hotmail.com>
2017-06-02 11:09:40 -0300

    fix port bind

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ let app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
