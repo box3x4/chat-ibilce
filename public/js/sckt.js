639aa8fab057f75bcc54dd24f47426b2cedaed78
Rafael Stefanini Carreira <rafael_carreira11@hotmail.com>
2017-07-10 23:50:32 -0300

    Update sckt.js

--- public/js/sckt.js
+++ public/js/sckt.js
@@ -208,7 +208,7 @@ $(function() {
     connected = true;
 
     // Display the welcome message
-    var message = "Bem-vindo ao Chat de "+ capitalize(namespace) + ". " + numUsers "usuários online";
+    var message = "Bem-vindo ao Chat de "+ capitalize(namespace) + ". " + numUsers + "usuários online";
     log(message, {
       prepend: true
     });
